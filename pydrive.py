38a59ee9ee5cf3e58729e1d840b02fc7aa5de1b3
We4theReport <alessandro.mastrelli@hotmail.com>
2018-04-26 15:18:07 +0200

    Update pydrive.py

--- pydrive.py
+++ pydrive.py
@@ -132,7 +132,7 @@ class PyDrive:
         
     def download(self, args):
         print(color.GREEN + "Downloading... " + args.file + color.ENDC)
-	    request = self.drive_service.files().get_media(fileId=self.fileid)
+	    request = self.drive_service.files().get_media(fileId=args.file)
 	    fh = io.BytesIO()
 	    downloader = MediaIoBaseDownload(fh, request)
 	    done = False
