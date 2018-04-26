5ea5e0983ea89668a6329847a09c1ab51842ff8b
We4theReport <alessandro.mastrelli@hotmail.com>
2018-04-26 21:46:15 +0200

    Update pydrive.py

--- pydrive.py
+++ pydrive.py
@@ -90,7 +90,7 @@ class PyDrive:
         parser.add_argument("-f", "--file", metavar="<file>", dest="file",
                             default=None, help="Specify a file")
         parser.add_argument("-a", "--action", metavar="<action>", dest="action",
-                            default=None, help="Specify an action ex.[upload, download, find, list]")
+                            default=None, help="Specify an action ex.[upload, download, find, list, delete]")
         args = parser.parse_args()
         if not args.action:
             print((color.RED) + ("Missing '-a' or '--action' argument!") + (color.ENDC))
@@ -99,9 +99,16 @@ class PyDrive:
             if not args.file:
                print((color.RED) + ("Missing '-f' or '--file' argument!") + (color.ENDC))
                sys.exit(-1)
-            if not os.path.exists(args.file):
-               print((color.RED) + ("The file doesn't exists!") + (color.ENDC))
-               sys.exit(-1)
+	    if ',' in args.file:
+	       files = args.file.split(',')
+	       for f in files:
+		    if not os.path.exists(f):
+		       print((color.RED) + ("The file doesn't exists!") + (color.ENDC))
+		       sys.exit(-1)
+	    else:
+		if not os.path.exists(args.file):
+		       print((color.RED) + ("The file doesn't exists!") + (color.ENDC))
+		       sys.exit(-1)
             self.upload(args)
         elif args.action == "download":
             if not args.file:
@@ -115,34 +122,65 @@ class PyDrive:
                print((color.RED) + ("Missing '-f' or '--file' argument!") + (color.ENDC))
                sys.exit(-1)
             self.search(args)
+	elif args.action == "delete":
+	    if not args.file:
+	       print((color.RED) + ("Missing '-f' or '--file' argument!") + (color.ENDC))
+               sys.exit(-1)
+            self.delete(args)
         else:
             print((color.RED) + ("Missing command argument!") + (color.ENDC))
             sys.exit(-1)
         return args
 
     def upload(self, args):
-        print(color.RED + "Uploading..." + args.file + color.ENDC)
-	    file_metadata = {'name': self.filename}
-	    media = MediaFileUpload(self.filepath,
-                        	    mimetype=self.mymetype)
-	    file = drive_service.files().create(body=file_metadata,
-                                    	    media_body=media,
-                                    	    fields='id').execute()
-	    print('File ID: %s' % file.get('id'))
+	files = args.file.split(',')
+	for f in files:
+        	print(color.RED + "Uploading " + f + color.ENDC)
+		filename = os.path.basename(f)
+		file_metadata = {'name': filename}
+		media = MediaFileUpload(f,
+		                	mimetype='application/octet-stream')
+		file = self.drive_service.files().create(body=file_metadata,
+		                            	media_body=media,
+		                            	fields='id').execute()
+		print('File Name: %s\nFile ID: %s' % (file.get('name'),file.get('id')))
         
     def download(self, args):
-        print(color.GREEN + "Downloading... " + args.file + color.ENDC)
-	    request = self.drive_service.files().get_media(fileId=args.file)
-	    fh = io.BytesIO()
-	    downloader = MediaIoBaseDownload(fh, request)
-	    done = False
-	    while done is False:
-	        status, done = downloader.next_chunk()
-	        print("Download %d%%." % int(status.proress() * 100))
-	    with io.open(self.filepath,'wb') as f:
-	        fh.seek(0)
-	        f.write(fh.read())
-		
+	print(color.GREEN + "Downloading... " + args.file + color.ENDC)
+	results = self.drive_service.files().list(
+            pageSize=10,fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        if not items:
+            print('No files found.')
+        else:
+	    for item in items:
+		if item['name'] == args.file:
+			request = self.drive_service.files().get_media(fileId=item['id'])
+			fh = io.BytesIO()
+			downloader = MediaIoBaseDownload(fh, request)
+			done = False
+			while done is False:
+			    status, done = downloader.next_chunk()
+			    print("Downloading %d%%." % int(status.progress() * 100))
+			directory = os.path.join(os.getcwd(), 'downloads')
+			if not os.path.exists(directory):
+    			    os.makedirs(directory)			
+			with io.open(os.path.join(directory, item['name']),'wb') as f:
+			    fh.seek(0)
+			    f.write(fh.read())
+				
+     def search(self, args):
+	print(color.BLUE + "Searching... " + args.file + color.ENDC)
+	results = self.drive_service.files().list(
+            pageSize=10,fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        if not items:
+            print('No files found.')
+        else:
+	    for item in items:
+		if item['name'] == args.file:
+		        print('File trovato! Ha come ID {0}'.format(item['id']))
+			
     def list(self, args):
         results = self.drive_service.files().list(
             pageSize=10,fields="nextPageToken, files(id, name)").execute()
@@ -156,6 +194,19 @@ class PyDrive:
                 data.append([item['name'],item['id']])
             t = SingleTable(data) #AsciiTable()
             print(t.table)
+
+    def delete(self, args):
+	print(color.BLUE + "delete" + args.file + color.ENDC)
+	results = self.drive_service.files().list(
+            pageSize=10,fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        if not items:
+            print('No files found.')
+        else:
+	    for item in items:
+		if item['name'] == args.file:
+			self.drive_service.files().delete(fileId=item['id']).execute()
+		        print('File cancellato!')
 
 if __name__ == '__main__':
     color = Colors()
